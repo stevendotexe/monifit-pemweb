bc2fcef99816a9448f16e5f23bec3a922c06f501
Rafie Afkar <afkar.yunansyah@gmail.com>
2025-06-14 21:06:57 +0700

    fix app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Monifit</title>
-</head>
-<body>
-    
-</body>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title inertia>{{ config('app.name', 'MoniFit') }}</title>
+
+        @routes
+        @viteReactRefresh
+        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+        @inertiaHead
+    </head>
+    <body class="font-sans antialiased">
+        @inertia
+    </body>
 </html>
